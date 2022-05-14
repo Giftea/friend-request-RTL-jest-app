f51d04fd20a2b8c4c6d2c3207b7b11e403935be3
Giftea <eleojogiftuhiene@gmail.com>
2022-05-14 11:51:03 +0100

    initial setup

--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -1,23 +1,7 @@
-const mockResponse = {
-    data: {
-      results: [
-        {
-          name: {
-            first: "gift",
-            last: "ele",
-          },
-          picture: {
-            large: "https://ip.jpg",
-          },
-          login: {
-            username: "ji",
-          },
-        },
-      ],
-    },
-  };
-  
-  export default {
-    get: jest.fn().mockResolvedValue(mockResponse),
-  };
-  
+import React from "react";
+
+const Request = () => {
+  return <div></div>;
+};
+
+export default Request;
